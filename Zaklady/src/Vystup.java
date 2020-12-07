78ef58c1549078b494191d9a0759d72685a312b8
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2020-12-07 14:59:38 +0100

    vystup programu
    println, print

---
+++ Zaklady/src/Vystup.java
@@ -0,0 +1,25 @@
+
+public class Vystup {
+
+	public static void main(String[] args) {
+		
+		System.out.println("elct.sk");
+		System.out.println("elct" + ".sk");
+		System.out.println(10 + 5);
+		System.out.println("10" + "5");
+		System.out.println(10 * 3);
+		System.out.println("Janko\nHrasko");
+		System.out.println("\"Janko\"");
+		System.out.println("\'Janko\'");
+		System.out.println("\\Janko\\");
+		System.out.println("--------");
+		System.out.println("Meno\tPriezvisko");
+		System.out.println("Fero\tMravec");
+		System.out.println("Janko\tHrasko");
+		System.out.println("-----------");
+		System.out.print("Ivan");
+		System.out.print("\nHrozny");
+
+	}
+
+}
