6e003e89782eca756298907b411922db7a7c21dd
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2020-12-07 14:21:17 +0100

    komentare

--- Zaklady/src/MojaPrva.java
+++ Zaklady/src/MojaPrva.java
@@ -1,10 +1,25 @@
-
 public class MojaPrva {
 
 	public static void main(String[] args) {
 		
+		//FIXME toto nefunguje
+		//toto je jednoriadkovy komentar
+		System.out.println("Lukas");
+		/*
+		 * toto 
+		 * je viacriadkovy
+		 * komentar
+		 */
+		
+		/**
+		 * javadoc komentar
+		 */
+		
+		//TODO toto musim dorobit
 		System.out.println("Ahoj svet");
-		System.out.println("jskdjfkhsdljkh");
+		
+		
+		
 
 	}
 
