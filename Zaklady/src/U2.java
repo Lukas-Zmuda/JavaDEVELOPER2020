ba4dd470b77a7ea618848f0e4fac332be8142fb5
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2020-12-08 10:51:07 +0100

    uloha premenne

---
+++ Zaklady/src/U2.java
@@ -0,0 +1,13 @@
+
+public class U2 {
+
+	public static void main(String[] args) {
+		//deklarovat a inicializovat 
+		//3 celociselne premenne
+		//3 desatinne premenne
+		//3 znakove premenne
+		//obsah premennej vypisat na konzolu v tvare "x = 12"
+
+	}
+
+}
