1cc902071223c4b6b611aa802a6a5eb14252cbab
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2020-12-15 10:14:42 +0100

    bmi upravene o vetvenie

--- Zaklady/src/U4BMI.java
+++ Zaklady/src/U4BMI.java
@@ -10,14 +10,37 @@ public class U4BMI {
 		System.out.println("--- Program na vypocet BMI ---");
 		System.out.println("Zadaj vysku[cm]: ");
 		int vyska = sc.nextInt(); //cm
+		if(!((vyska > 40) && (vyska < 280))) {
+			System.out.println("Nespravna vyska! Spusti program este raz.");
+			System.exit(0);
+		}
 		System.out.println("Zadaj hmotnost[kg]: ");
 		int hmotnost = sc.nextInt(); //kg
-
+		if(!((hmotnost > 10) && (hmotnost < 650))) {
+			System.out.println("Nespravna hmotnost! Spusti program este raz.");
+			System.exit(0);
+		}
 		double v = vyska / 100.0;
 		double bmi = hmotnost / (v * v);
 		
 		System.out.println("Hmotnost: " + hmotnost + " kg");
 		System.out.println("Vyska: " + vyska + " cm");
-		System.out.println("BMI = " + bmi);		
+		System.out.println("BMI = " + bmi);	
+		if(bmi < 17.5) {
+			System.out.println("Podvyziva");
+		}
+		if((bmi >= 17.5) && (bmi < 25)) {
+			System.out.println("Idealna a zdrava vaha");
+		}
+		if((bmi >= 25) && (bmi < 30)) {
+			System.out.println("Mierna nadvaha");
+		}
+		if((bmi >= 30) && (bmi < 40)) {
+			System.out.println("Obezita");
+		}
+		if(bmi > 40) {
+			System.out.println("Tazka obezita");
+		}
+		
 	}
 }
