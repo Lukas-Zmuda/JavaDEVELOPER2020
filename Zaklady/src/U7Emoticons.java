8f63d60a9efeda47fb328df16f56c50934f7c2e7
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2020-12-15 11:24:35 +0100

    uloha emotikon

---
+++ Zaklady/src/U7Emoticons.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class U7Emoticons {
+
+	public static void main(String[] args) {
+		//Program na vstupe nacita emotikon a vypise emociu uzivatela
+		//napr. -> :-)
+		//<- usmev
+		//-> :(
+		//<- smutny
+		//5 emocii; akceptovat emotikony s nosom aj bez neho
+	
+		String emtkn = "";
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Zadaj emotikon: ");
+		emtkn = sc.nextLine();
+		switch(emtkn) {
+		case ":-)":
+		case ":)":
+			System.out.println("Usmev");
+			break;
+		case ":-O":
+		case ":O":
+			System.out.println("Prekvapenie");
+			break;
+		case ":-(":
+		case ":(":
+			System.out.println("Smutny");
+			break;
+		case ":-P":
+		case ":P":
+			System.out.println("Vyplazeny jazyk");
+			break;
+		case ":-@":
+		case ":@":
+			System.out.println("Nahnevany");
+			break;
+		default:
+			System.out.println("Nepoznam emotikon");
+		}
+
+	}
+
+}
