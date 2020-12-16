95b489cdb5fc71e7f993f1679f097aae7a659fbc
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2020-12-16 14:26:10 +0100

    vnorene cykly

---
+++ Zaklady/src/VnorenyCyklus.java
@@ -0,0 +1,28 @@
+
+public class VnorenyCyklus {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 10; j++) {
+				System.out.println(i + ":" + j);
+			}
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("-----------");
+		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
