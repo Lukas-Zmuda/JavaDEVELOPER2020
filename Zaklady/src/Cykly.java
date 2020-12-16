a5ea845a60f10dc0214ce4c73ae2c5a79cf5ee9d
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2020-12-16 09:58:17 +0100

    cykly

---
+++ Zaklady/src/Cykly.java
@@ -0,0 +1,25 @@
+
+public class Cykly {
+
+	public static void main(String[] args) {
+		
+//		System.out.println("Ahoj");
+//		System.out.println("Ahoj");
+//		System.out.println("Ahoj");
+//		System.out.println("Ahoj");
+//		System.out.println("Ahoj");
+
+		//cyklus s pevnym poctom opakovani
+		for(int i = 0; i < 5; i++) {
+			System.out.println((i + 1) + ". Ahoj");
+		}				
+		System.out.println("Ako sa mas?");
+
+		for(int i = 10; i > 0; i--) {
+			System.out.print(i + ", ");
+		}
+		
+		
+	}
+
+}
